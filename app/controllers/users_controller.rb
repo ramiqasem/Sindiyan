b82b5fcd992cd735236d8d20d978119e1959e988
Rami Hussein <ramiqasem@gmail.com>
2015-06-04 18:30:46 +0300

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user=User.new(user_params)
       	if @user.save
           
-          UserMailer.account_activation(@user).deliver_now
+          #UserMailer.account_activation(@user).deliver_now
           #flash[:info] = "Please check your email to activate your account."
           #redirect_to root_url
           log_in @user
