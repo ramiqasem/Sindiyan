04d19cac554c81c652d3c93424b15d0a56f4b2ce
Rami Hussein <ramiqasem@gmail.com>
2015-06-10 21:43:51 +0300

    whatever

---
+++ public/assets/pagination-2185852006db319dee99f2558cfff1d2.js
@@ -0,0 +1,20 @@
+$(function(){
+	
+	$( ".pagination a").click(function(){
+		$('#flash_hook').empty();
+		var num = $('div[id="tabs"] .tab-pane.active').attr('id') 
+		$.get(this.href, { id: num }, null, "script");
+		return false;
+	
+		
+
+
+});
+
+	$("button:contains('show more')").click(function(){
+		$(this).parent().find(".pagination a[rel=\'next\']").click();
+		return false;
+	});
+
+
+});
