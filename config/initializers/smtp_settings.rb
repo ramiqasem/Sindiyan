8f4517766d087ba8a5b3d31dcf812c230f4053b5
ramiqasem <ramiqasem@gmail.com>
2016-02-24 17:24:21 +0200

    Update smtp_settings.rb

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -3,8 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain				=> "Sindiyan.com",
-  :user_name            => "ramiqasem@gmail.com",
-  :password             => "mayanmj777",
+
   :authentication       => :plain,
   :enable_starttls_auto => true
   
